4cb3bb736af7bdf4408bfca1309e0d0a553228ad
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-17 10:51:57 -0300

    Os inputs já estão captando os valors e armazenando em suas respectivas states na página de gerenciar link

---
+++ src/app/admin/gerenciar-links/Listagem.tsx
@@ -0,0 +1,39 @@
+export default function Listagem() {
+  return (
+    <div className="links">
+
+        <div className="link">
+          <h3><i className="fa-brands fa-youtube" aria-hidden="true"></i> Youtube</h3>
+          <div className="botoes">
+            <i className="fa-solid fa-pen-to-square fa-lg" aria-hidden="true"></i>
+            <i className="fa-solid fa-trash fa-lg" aria-hidden="true"></i>
+          </div>
+        </div>
+
+        <div className="link">
+          <h3> <i className="fa-brands fa-linkedin" aria-hidden="true"></i> Linkedin</h3>
+          <div className="botoes">
+            <i className="fa-solid fa-pen-to-square fa-lg" aria-hidden="true"></i>
+            <i className="fa-solid fa-trash fa-lg" aria-hidden="true"></i>
+          </div>
+        </div>
+
+        <div className="link">
+          <h3> <i className="fa-brands fa-github" aria-hidden="true"></i> Github</h3>
+          <div className="botoes">
+            <i className="fa-solid fa-pen-to-square fa-lg" aria-hidden="true"></i>
+            <i className="fa-solid fa-trash fa-lg" aria-hidden="true"></i>
+          </div>
+        </div>
+
+        <div className="link">
+          <h3> <i className="fa-solid fa-display" aria-hidden="true"></i> Portfólio</h3>
+          <div className="botoes">
+            <i className="fa-solid fa-pen-to-square fa-lg" aria-hidden="true"></i>
+            <i className="fa-solid fa-trash fa-lg" aria-hidden="true"></i>
+          </div>
+        </div>
+      
+    </div>
+  )
+}
--- src/app/admin/gerenciar-links/page.tsx
+++ src/app/admin/gerenciar-links/page.tsx
@@ -1,18 +1,28 @@
+'use client'
+
+import { useState } from "react"
 import "./gerenciar-links.css"
+import Listagem from "./Listagem"
+
+type TipoAlias = "Linkedin" | "Linkedin" | "Github" | "Portfólio" | ""
 
 export default function GerenciarLinks() {
+  const [caminho, setCaminho] = useState<string>("")
+  const [tipo, setTipo] = useState<TipoAlias>("")
+
   return (
     <section className="gerenciar-links">
       <h1> <i className="fa-solid fa-gear" aria-hidden="true"></i> Gerencie seus Links</h1>
       <form>
-
+        <h2>{tipo}</h2>
         <input 
           placeholder="Cole o caminho do link"
           type="text"
+          onChange={(e)=>setCaminho(e.target.value)}
           required
         />
 
-        <select defaultValue={""}>
+        <select onChange={(e)=>setTipo(e.target.value)}>
           <option value="" disabled selected>Selecione o tipo do link que deseja adicionar</option>
           <option value="Youtube">Youtube</option>
           <option value="Linkedin">Linkedin</option>
@@ -20,47 +30,13 @@ export default function GerenciarLinks() {
           <option value="Portfólio">Portfólio</option>
         </select>
 
-        <button>Salvar alterações</button>
+        <button>Adicionar</button>
 
       </form>
 
       <h2> <i className="fa-solid fa-link" aria-hidden="true"></i> Seus links</h2>
+      <Listagem />
 
-      <div className="links">
-
-        <div className="link">
-          <h3><i className="fa-brands fa-youtube" aria-hidden="true"></i> Youtube</h3>
-          <div className="botoes">
-            <i className="fa-solid fa-pen-to-square fa-xl" aria-hidden="true"></i>
-            <i className="fa-solid fa-trash fa-xl" aria-hidden="true"></i>
-          </div>
-        </div>
-
-        <div className="link">
-          <h3> <i className="fa-brands fa-linkedin" aria-hidden="true"></i> Linkedin</h3>
-          <div className="botoes">
-            <i className="fa-solid fa-pen-to-square fa-xl" aria-hidden="true"></i>
-            <i className="fa-solid fa-trash fa-xl" aria-hidden="true"></i>
-          </div>
-        </div>
-
-        <div className="link">
-          <h3> <i className="fa-brands fa-github" aria-hidden="true"></i> Github</h3>
-          <div className="botoes">
-            <i className="fa-solid fa-pen-to-square fa-xl" aria-hidden="true"></i>
-            <i className="fa-solid fa-trash fa-xl" aria-hidden="true"></i>
-          </div>
-        </div>
-
-        <div className="link">
-          <h3> <i className="fa-solid fa-display" aria-hidden="true"></i> Portfólio</h3>
-          <div className="botoes">
-            <i className="fa-solid fa-pen-to-square fa-xl" aria-hidden="true"></i>
-            <i className="fa-solid fa-trash fa-xl" aria-hidden="true"></i>
-          </div>
-        </div>
-      
-      </div>
     </section>
   )
 }
