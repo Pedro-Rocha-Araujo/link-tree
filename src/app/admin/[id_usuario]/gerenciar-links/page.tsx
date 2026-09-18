38ad607f2e508d7cb6a69d946491aaa4b7efc309
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-18 08:29:34 -0300

    Adicionei a funcionalidade de cadastrar um link

--- src/app/admin/[id_usuario]/gerenciar-links/page.tsx
+++ src/app/admin/[id_usuario]/gerenciar-links/page.tsx
@@ -3,6 +3,10 @@
 import { useState } from "react"
 import "./gerenciar-links.css"
 import Listagem from "./Listagem"
+import { toast } from "react-toastify"
+import db from "@/FirebaseConnection"
+import { addDoc, collection } from "firebase/firestore"
+import { useParams } from "next/navigation"  
 
 type TipoAlias = "Linkedin" | "Linkedin" | "Github" | "Portfólio" | ""
 
@@ -10,19 +14,44 @@ export default function GerenciarLinks() {
   const [caminho, setCaminho] = useState<string>("")
   const [tipo, setTipo] = useState<TipoAlias>("")
 
+  const { id_usuario } = useParams()
+
+  async function cadastrarLink(e: React.FormEvent) {
+    e.preventDefault()
+    try {
+      if(!caminho || !tipo) {
+        toast.error("Erro!")
+        return
+      }
+      const ref = collection(db, "links")
+      await addDoc(ref, {
+        caminho: caminho,
+        tipo: tipo,
+        id_usuario: id_usuario
+      })
+      setCaminho("")
+      setTipo("")
+      toast.success("Link cadastrado.")
+    } catch(erro) {
+      console.log(erro) 
+      toast.error("Erro!")
+    }
+  }
+
   return (
     <section className="gerenciar-links">
       <h1> <i className="fa-solid fa-gear" aria-hidden="true"></i> Gerencie seus Links</h1>
-      <form>
+      <form onSubmit={cadastrarLink}>
         <input 
           placeholder="Cole o caminho do link"
           type="text"
+          value={caminho}
           onChange={(e)=>setCaminho(e.target.value)}
           required
         />
 
-        <select onChange={(e)=>setTipo(e.target.value)}>
-          <option value="" disabled selected>Selecione o tipo do link que deseja adicionar</option>
+        <select defaultValue={tipo} onChange={(e)=>setTipo(e.target.value)}>
+          <option value="" disabled selected >Selecione o tipo do link que deseja adicionar</option>
           <option value="Youtube">Youtube</option>
           <option value="Linkedin">Linkedin</option>
           <option value="Github">Github</option>
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,10 @@ import { NextRequest, NextResponse } from "next/server"
 export default function middleware(request:NextRequest) {
   const token = request.cookies.get("token")?.value
   const urlAdmin = request.nextUrl.pathname.startsWith("/admin")
+  const nomeRota = request.nextUrl.pathname
+  if(nomeRota === "/") {
+    return NextResponse.redirect(new URL("/auth/login", request.url))
+  }
   if(!token && urlAdmin) {
     return NextResponse.redirect(new URL("/auth/login", request.url))
   }
